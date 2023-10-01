95fb0ab1bd040923e423b0c718dc8bc83f97081a
MR.Unknown <133843985+Sandeepmmv@users.noreply.github.com>
2023-10-01 18:10:09 +0530

    main.dart

---
+++ 2-Designing the mobile app to implement different widgets/main.dart
@@ -0,0 +1,49 @@
+practical no. 2 
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LayoutApp());
+}
+
+class LayoutApp extends StatelessWidget {
+  const LayoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Layout App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(decoration: InputDecoration(labelText: 'Username')),
+            const TextField(decoration: InputDecoration(labelText: 'Password')),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement login functionality
+              },
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
